2ec64f89e84f9e3ab4635d9fce2986022e58ad23
schennamon <mr.tolokneev@gmail.com>
2022-01-22 18:03:55 +0200

    Implemented output of messages from the db and modified navigation views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Olivia", password_digest: "password")
+User.create(username: "John", password_digest: "password")
+User.create(username: "Ivan", password_digest: "password")
+User.create(username: "Tabasco", password_digest: "password")
+User.create(username: "Mikhail", password_digest: "password")
